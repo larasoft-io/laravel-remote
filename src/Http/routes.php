874db2445bcd055823f20efabe62ca0b1fcb87ae
Fahad Ali <rock.star9722@gmail.com>
2016-09-29 19:22:15 +0500

    Update .env from laravelremote-web done

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,13 +2,18 @@
 
     // Test the connection with Laravel Remote
     Route::get(
-        'laravel-remote/chstatus',
+        'laravel-remote/status/check',
         '\Larasoft\LaravelRemote\Http\Controllers\LaravelRemoteController@getStatus'
     );
 
     Route::get(
         'laravel-remote/env/variables',
         '\Larasoft\LaravelRemote\Http\Controllers\LaravelRemoteController@getEnvVariables'
+    );
+
+    Route::post(
+        'laravel-remote/env/variable',
+        '\Larasoft\LaravelRemote\Http\Controllers\LaravelRemoteController@updateEnvVariable'
     );
 
     // Execute commands received from Laravel Remote
